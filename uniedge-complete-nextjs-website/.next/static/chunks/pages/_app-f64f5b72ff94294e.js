3b90bcc9ef69c7efa659738b42ad25b634b702aa
Sylvertz <donsylguy@gmail.com>
2025-07-22 11:11:04 +0000

    SilverG

---
+++ uniedge-complete-nextjs-website/.next/static/chunks/pages/_app-f64f5b72ff94294e.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[636],{8646:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(6320)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[593,792],()=>(n(8646),n(4574))),_N_E=_.O()}]);
